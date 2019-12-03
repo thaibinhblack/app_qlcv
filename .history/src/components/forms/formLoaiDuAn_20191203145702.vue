80735154926b77c1eaf5e7fe096e5d9216e7ab41
thaibinhblack <thaibinhblack@gmail.com>
2019-12-03 17:01:04 +0700

    deploy

---
+++ .history/src/components/forms/formLoaiDuAn_20191203145702.vue
@@ -0,0 +1,119 @@
+<template>
+ <form @submit.prevent="api_loai_du_an()" class="form-rule" style="padding: 15px;">
+    <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-4 col-form-label" >Tên loại dự án</label>
+        <div class="col-sm-8">
+            <b-input type="text" v-model="loai_du_an.ten_loai_da"  placeholder="Tên loại dự án" required></b-input>
+        </div>
+    </div>
+        <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-4 col-form-label" >Mô tả</label>
+        <div class="col-sm-8">
+            <b-input type="textarea" v-model="loai_du_an.mo_ta" minlength=""
+            maxlength="255" placeholder="Mô tả loại dự án"  ></b-input>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-4 col-form-label" >Ghi chú</label>
+        <div class="col-sm-8">
+            <b-input type="text" v-model="loai_du_an.ghi_chu"  placeholder="Ghi chú"  ></b-input>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-4 col-form-label" >Trạng thái</label>
+        <div class="col-sm-8">
+            <b-field>
+                <b-select v-model="loai_du_an.trang_thai">
+                    <option value="1" selected>Đang hoạt động</option>
+                    <option value="2">Đang bảo trì</option>
+                    <option value="3">Ngừng hoạt động</option>
+                </b-select>
+            </b-field>
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col-sm-9">
+            <button type="submit" :disabled="update"  class="btn btn-primary btn-form">Thêm</button>
+            <button type="button" :disabled="!update" class="btn btn-warning btn-form" @click="updateLoaiDuAn()">Cập nhật</button>
+            <button type="submit" v-if="update == true" @click="$emit('clear',false)" class="btn btn-success btn-form">Clear</button>
+        </div>
+    </div>
+</form>
+</template>
+
+<script>
+export default {
+    props: ["update", "loai_du_an_edit"],
+    data()
+    {
+        return {
+            loai_du_an: {
+                trang_thai: 1
+            }
+        }
+    },
+    watch:
+    {
+        loai_du_an_edit(newVal)
+        {
+            console.log(newVal)
+            this.loai_du_an = newVal
+        },
+        update(newVal)
+        {
+            if(newVal == false)
+            {
+                this.loai_du_an =  {
+                    trang_thai: 1
+                }
+            }
+        }
+    },
+    methods: {
+        api_loai_du_an()
+        {
+            const loai_du_an = new FormData();
+            loai_du_an.append("P_TEN_LOAI_DA", this.loai_du_an.ten_loai_da);
+            loai_du_an.append("P_MO_TA", this.loai_du_an.mo_ta);
+            loai_du_an.append("P_GHI_CHU", this.loai_du_an.ghi_chu);
+            loai_du_an.append("P_TRANG_THAI", this.loai_du_an.trang_thai);
+            const app = this;
+            this.axios.post(this.$store.state.config.API_URL + 'loai-du-an?api_token='+this.$cookies.get('token'),loai_du_an).then((response) => {
+                app.loai_du_an = {
+                    trang_thai: 1
+                }
+                app.$buefy.notification.open({
+                    duration: 1500,
+                    message: response.data.message,
+                    position: 'is-bottom-right',
+                    type: 'is-success',
+                    hasIcon: true
+                })
+                app.$emit('clear',false)
+            })
+        },
+        updateLoaiDuAn()
+        {
+            const loai_du_an = new FormData();
+            loai_du_an.append("P_TEN_LOAI_DA", this.loai_du_an.ten_loai_da);
+            loai_du_an.append("P_MO_TA", this.loai_du_an.mo_ta);
+            loai_du_an.append("P_GHI_CHU", this.loai_du_an.ghi_chu);
+            loai_du_an.append("P_TRANG_THAI", this.loai_du_an.trang_thai);
+            const app = this;
+            this.axios.post(this.$store.state.config.API_URL + 'loai-du-an/'+this.loai_du_an.id_loai_da+'?api_token='+this.$cookies.get('token'),loai_du_an).then((response) => {
+                app.loai_du_an = {
+                    trang_thai: 1
+                }
+                app.$buefy.notification.open({
+                    duration: 1500,
+                    message: response.data.message,
+                    position: 'is-bottom-right',
+                    type: 'is-success',
+                    hasIcon: true
+                })
+                this.$emit('clear',false)
+            })
+        }
+    }
+}
+</script>
---
+++ .history/src/pages/managers/phongban/danhsachPhongBan_20191203143755.vue
@@ -0,0 +1,142 @@
+<template>
+<div id="page-ds-phong-ban">
+    <div class="row">
+        <div class="col-sm-12 col-md-8 col-lg-7" style="margin:auto">
+            <div class="card">
+                <p class="background">Danh mục phòng ban</p>
+                <form-group  :trung_tams="trung_tams" @create_pb="create_phong_ban = $event" :phong_ban_edit="phong_ban_edit"  :updatePhongBan="updatePhongBan"
+                @rollback="updatePhongBan = $event" :update="update"/>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="table-phong-ban" style="margin-top: 50px" oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+        <p class="background">Danh sách phòng ban</p>
+        <JqxGrid ref="myGrid"
+                :width="'99%'" :source="dataAdapter" :columns="columns" 
+                :filterable="true" :selectionmode="'multiplecellsextended'" :showfilterrow="true" @rowclick="onRowclick($event)">
+        </JqxGrid>
+    </div>
+</div>
+</template>
+
+<script>
+import JqxGrid from "jqwidgets-scripts/jqwidgets-vue/vue_jqxgrid.vue";
+
+export default {
+    components: {
+        'form-group': () => import('@/components/forms/formgroup.vue'),
+        JqxGrid
+    },
+    data()
+    {
+        return {
+            trung_tams: [],
+            dataAdapter: new jqx.dataAdapter(this.source),
+            columns: [
+                {text: 'ID_TT', datafield: 'idtt', hidden: true},
+                {text: 'ID_NHOM', datafield: 'id_nhom', hidden: true},
+                {text: 'Tên trung tâm', datafield: 'ten_tt', width: 240, filtertype: 'checkedlist'},
+                {text: 'Tên phòng ban', datafield: 'ten_nhom', width: 240},
+                {text: 'Mô tả', datafield: 'mo_ta_nhom', width: 240},
+                {text: 'Ghi chú', datafield: 'ghi_chu_nhom', width: 240},
+                {text: 'Trạng thái', datafield: 'trang_thai_nhom', width: 240, filtertype: 'checkedlist'},
+                {text: 'Ngày tạo', datafield: 'created_at', width: 240},
+                
+               
+            ],
+            popup_active: false,
+            create_phong_ban: false,
+            positon: {
+                top: '33px',
+                left: '0px'
+            },
+            phong_ban_edit: {},
+            groups: [],
+            updatePhongBan: false,
+            update: false
+        }
+    },
+    watch:
+    {
+        updatePhongBan(newVal)
+        {
+            if(newVal == true)
+            {
+                this.popup_active = false
+            }
+        },
+        create_phong_ban(newVal)
+        {
+            if(newVal == true)
+            {
+                this.create_phong_ban = false
+                this.source.localdata = {
+                    datatype: "json",
+                    datafields: [
+                   
+                    { name: 'ten_tt', type: 'string'},
+                    { name: 'id_nhom', type: 'number'},
+                    { name: 'ten_nhom' , type: 'string' },
+                    { name: 'mo_ta_nhom', type: 'string' },
+                    { name: 'ghi_chu_nhom' ,type: 'string'},
+                    { name: 'trang_thai_nhom', type: 'number' },
+                    { name: 'created_at', type: 'date'},
+                    { name: 'id_tt', type: 'number'}
+                    ],
+                    url: this.$store.state.config.API_URL + 'group_user?api_token='+this.$cookies.get('token'),
+                    
+                }
+                // passing 'cells' to the 'updatebounddata' method will refresh only the cells values when the new rows count is equal to the previous rows count.
+                this.$refs.myGrid.updatebounddata('cells');
+            }
+        }
+    },
+    methods: {
+        api_trung_tams()
+        {
+            this.axios.get(this.$store.state.config.API_URL + 'trung-tam?api_token='+this.$cookies.get("token")).then((response) => {
+                this.trung_tams = response.data
+            })
+        },
+        onRowclick: function (event) {
+            
+             this.updatePhongBan = true
+            this.phong_ban_edit = event.args.row.bounddata
+            
+        },
+        api_phong_ban()
+        {
+            this.axios.get(this.$store.state.config.API_URL + 'group_user?api_token='+this.$cookies.get('token')).then((response) => {
+                this.groups = response.data
+            })
+        },
+    },
+    beforeCreate: function () {
+        this.source = {
+            datatype: "json",
+            datafields: [
+                { name: 'id_tt', type: 'number'},
+                { name: 'id_nhom', type: 'number'},
+                { name: 'ten_tt', type: 'string'},
+                { name: 'ten_nhom' , type: 'string' },
+                { name: 'mo_ta_nhom', type: 'string' },
+                { name: 'ghi_chu_nhom' ,type: 'string'},
+                { name: 'trang_thai_nhom', type: 'number' },
+                { name: 'created_at', type: 'date'}
+            ],
+            url: this.$store.state.config.API_URL + 'group_user?api_token='+this.$cookies.get('token'),
+            
+            }
+    },
+    created()
+    {
+        this.api_trung_tams()
+    }
+}
+</script>
+
+<style scoped>
+.table-phong-ban {position: relative;}
+</style>
